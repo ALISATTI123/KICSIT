e885c21fd581a0f45cd80aaba7de6fadaa31df64
ALISATTI123 <XYZ@example.com>
2026-02-27 13:26:31 +0500

    example

---
+++ testgithub/opps.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    innt a,b;
+    cout<<"enter two numbers :";
+    cin>>a>>b;
+    cout<<"sum of the two numbers is:"<<a+b;
+    return 0;
+}
